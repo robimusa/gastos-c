08c48ceb75d410115426663550ea441b2b8cb0aa
Robi Rob <rob@rob.com>
2020-02-04 18:22:25 +0100

    mongoDB bien conectado y funcionando

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 import os
 
+# CONEXION MONGODB + BASE DE DATOS + COLECCION
+URL_MONGO = 'mongodb+srv://Invitado:Guest5050@cluster0-3lrrr.mongodb.net/test?retryWrites=true&w=majority'
+cliente = MongoClient(URL_MONGO, ssl_cert_reqs=False)
+db = cliente['primeraBD']
+coleccion = db['gastoscoche']
 
 app = Flask(__name__)
 
+registro = {"prueba":"prueba"}
+# db.coleccion.insertOne(registro)
+coleccion.insert_one(registro)
 
 # RUTAS
 @app.route('/', methods=["GET","POST"])
